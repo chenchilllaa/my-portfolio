7e5e958e180e4fbbaa15f6a0c64bde2d4bc03f9f
Jessica Chen <jessicachen@jessicas-mbp.lan>
2018-12-27 15:03:37 -0800

    CSS Styles on Homepage

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Jchen Design" keywords={[`jessica`, `chen`, `design`]} />
+        <div>
+          <div className="Hero">
+            <div className="HeroGroup">
+              <h1>Night Owl</h1>
+              <p>Sleep is for the dead</p>
+              <Link to="/page-2/">Foshizzle</Link>
+            </div>
+          </div>
+         
+        </div>
+  </Layout>
+)
+
+export default IndexPage
